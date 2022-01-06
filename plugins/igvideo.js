c4c472414077f0b58a472e8935082b1ad633f905
RobbyCatur <79045499+RobbyCatur@users.noreply.github.com>
2022-01-06 23:57:17 +0700

    Update igvideo.js

--- plugins/igvideo.js
+++ plugins/igvideo.js
@@ -1,6 +1,6 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn, args }) => {
-  let res = await fetch('LeysCoder', '/api/instagram/video', { url: args[0] }, 'apikey'))
+  let res = await fetch(global.API('LeysCoder', '/api/instagram/video', { url: args[0] }, 'apikey'))
   let json = await res.json()
   let hasil = json.result
   conn.sendFile(m.chat, hasil, 'ig.mp4', m)
