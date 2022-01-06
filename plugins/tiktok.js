335bf7fcc14e01f67415bb712d89a501d4e18ed2
RobbyCatur <79045499+RobbyCatur@users.noreply.github.com>
2022-01-06 22:06:37 +0700

    Add files via upload

--- plugins/tiktok.js
+++ plugins/tiktok.js
@@ -3,14 +3,11 @@ let handler = async (m, { conn, args }) => {
 	m.reply('Proses')
   if (!args[0]) throw 'Uhm...url nya mana?'
   let me = conn.user.name
-  let res = await fetch(global.API('xteam', '/dl/tiktok', {
-    url: args[0]
-  }, 'APIKEY'))
-  if (res.status !== 200) throw `Coba lagi`
+  let res = await fetch(`https://api.lolhuman.xyz/api/tiktok?apikey=c6670fc7e461b7623a8fdf9f&url=` + args[0])
+//  if (res.status !== 200) throw `Coba lagi`
   let json = await res.json()
-  if (!json.status) throw json
-  let url = json.result.link_dl1 || json.result.link_dl2 || ''
-  if (!url) throw 'Gagal mengambil url download'
+//  if (!json.status) throw json
+  let url = json.result.link
   let txt = `
 ${me} Tiktok Downloader
     `
