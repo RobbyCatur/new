6f525567b05462c39bfd1929a23a2c10a2cadf54
RobbyCatur <79045499+RobbyCatur@users.noreply.github.com>
2022-01-06 23:52:35 +0700

    Add files via upload

--- plugins/ig.js
+++ plugins/ig.js
@@ -1,13 +1,5 @@
-let fetch = require('node-fetch')
-
 let handler = async (m, { conn, args }) => {
-  let me = await conn.user.name
-  let res = await fetch(global.API('LeysCoder', '/api/instagram/video', { url: args[0] }, 'apikey'))
-  let json = await res.json()
-  let url = json.result
-  if (!url) throw `URL tidak ditemukan`
-  await conn.sendFile(m.chat, url, 'ig.mp4', `${me} Instagram downloader`, m)
+  conn.send2Button(m.chat, `Silahkan pilih media`, '©Robby Catur', 'IG FOTO', `.igfoto ${args[0]}`, 'IG VIDEO', `.igvideo ${args[0]}`, m)
   }
   handler.command = /^ig$/i
-  module.exports = handler
-  
+module.exports = handler
---
+++ plugins/igfoto.js
@@ -0,0 +1,6 @@
+let fetch = require('node-fetch')
+let handler = async (m, { conn }) => {
+  conn.sendFile(m.chat, global.API('LeysCoder', '/api/instagram/photo', { url: args[0] }, apikey))
+  {
+  handler.command = /^igfoto$/i
+  module.exports = handler
