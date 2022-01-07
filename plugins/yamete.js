52bb8f9559921b8a6c7572f2b88b8bd812caacb4
RobbyCatur <79045499+RobbyCatur@users.noreply.github.com>
2022-01-07 21:30:58 +0700

    Update yamete.js

--- plugins/yamete.js
+++ plugins/yamete.js
@@ -2,12 +2,12 @@ let util = require('util')
 let path = require('path')
 
 let handler = async (m, { conn }) => {
-let vn ='src/sound/yamete.mp3'
+let vn ='./src/sound/yamete.mp3'
 conn.sendFile(m.chat, vn, 'yamete.mp3', null, m, true, {
 type: 'audioMessage', // paksa tanpa convert di ffmpeg
 ptt: true // true diatas ga work, sebab dipaksa tanpa convert ;v
 })
 }
-handler.customPrefix = /Yamete/
+handler.customPrefix = /yamete/
 handler.command = new RegExp
 module.exports = handler
