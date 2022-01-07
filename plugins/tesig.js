5fb18114f8b5590787e8faab66d28b88792831eb
RobbyCatur <79045499+RobbyCatur@users.noreply.github.com>
2022-01-07 10:08:36 +0700

    Add files via upload

--- plugins/tesig.js
+++ plugins/tesig.js
@@ -1,8 +1,19 @@
-const { igdl } = require('../lib/igdl-scraper')
-let handler = async (m, { conn, text, }) => {
-  let json = await igdl(text)
-  let result = json.data
- conn.sendFile(m.chat, result, 'ig.mp4', m)
- }
- handler.command = /^tesig$/i
+let fetch = require('node-fetch')
+
+let handler = async (m, { conn, args }) => {
+	let me = conn.user.name
+	let res = await fetch(`https://hardianto.xyz/api/instagram?url=` + args[0])
+	let json = await res.json()
+	let url = json.url
+	let isvideo = json.isvideo
+	let txt = `
+	${me} Instagram downloader
+Post Info
+Like : ${json.like}
+Komen : ${json.comment}
+View : ${json.view}
+`
+   conn.sendFile(m.chat, url, 'ig' + (isvideo == 'true' ? '.mp4', '.jpg'), txt.trim(), m)
+   }
+   handler.command = /^ig$/i
 module.exports = handler
