f4432c9c1eaf90e7578eb5b584130aa37abdd013
RobbyCatur <79045499+RobbyCatur@users.noreply.github.com>
2022-01-07 09:47:38 +0700

    Delete igvideo.js

--- plugins/igvideo.js
+++
@@ -1,9 +0,0 @@
-let fetch = require('node-fetch')
-let handler = async (m, { conn, args }) => {
-  let res = await fetch(global.API('LeysCoder', '/api/instagram/video', { url: args[0] }, 'apikey'))
-  let json = await res.json()
-  let hasil = json.result
-  conn.sendFile(m.chat, hasil, 'ig.mp4', m)
-  }
-  handler.command = /^igvideo$/i
-  module.exports = handler
