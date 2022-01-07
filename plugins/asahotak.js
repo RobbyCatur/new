72350a7be2e759a4cc44c9a512a757df9b60dcab
RobbyCatur <79045499+RobbyCatur@users.noreply.github.com>
2022-01-07 20:37:20 +0700

    Update asahotak.js

--- plugins/asahotak.js
+++ plugins/asahotak.js
@@ -9,7 +9,7 @@ let handler  = async (m, { conn, usedPrefix }) => {
         conn.reply(m.chat, 'Masih ada soal belum terjawab di chat ini', conn.asahotak[id][0])
         throw false
     }
-    let res = await fetch(global.API('xteam', '/game/asahotak', {}, 'APIKEY'))
+    let res = await fetch(`https://api.lolhuman.xyz/api/tebak/asahotak?apikey=` + apikey)
     if (res.status !== 200) throw await res.text()
     let json = await res.json()
     if (!json.status) throw json
