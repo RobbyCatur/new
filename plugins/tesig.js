6972ab1f4c7c524b50f857e36240f2d31ef96f00
RobbyCatur <79045499+RobbyCatur@users.noreply.github.com>
2022-01-07 12:13:42 +0700

    Update tesig.js

--- plugins/tesig.js
+++ plugins/tesig.js
@@ -1,11 +1,11 @@
 const HX = require('hxz-api')
 
-let handler = async (m, { conn }) => {
+let handler = async (m, { conn, text }) => {
   let me = conn.user.name
   let txt = `
 ${me} Instagram downloader
 `.trim()
-  let result = HX.igdl(url).then(res => conn.sendFile(m.chat, res.medias[0].url, '', '',txt, m))
+  let result = HX.igdl(text).then(res => conn.sendFile(m.chat, res.medias[0].url, '', '',txt, m))
 }
 handler.command = /^tesig$/i
 module.exports = handler
