cc0eb654f9d906f11b81536e99e97cea8cdb9864
RobbyCatur <79045499+RobbyCatur@users.noreply.github.com>
2022-01-07 09:46:56 +0700

    Delete ig.js

--- plugins/ig.js
+++
@@ -1,5 +0,0 @@
-let handler = async (m, { conn, args }) => {
-  conn.send2Button(m.chat, `Silahkan pilih media`, '©Robby Catur', 'IG FOTO', `.igfoto ${args[0]}`, 'IG VIDEO', `.igvideo ${args[0]}`, m)
-  }
-  handler.command = /^ig$/i
-module.exports = handler
